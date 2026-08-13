280257a0cee22781b9be4d034be671009b3ed970
Akshata S Hamasagar <105004258+Akshata153@users.noreply.github.com>
2026-08-13 20:56:05 +0530

    Add: sort-array-by-increasing-frequency - submission-1

---
+++ Data Structures & Algorithms/sort-array-by-increasing-frequency/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        mymap=Counter(nums)
+        arr=[]
+        #key, freq
+        nums.sort(key=lambda x:(mymap[x],-x))
+        # each elemnet of nums parsed and looked into its scoring tupple  (freq, -key)and is sorted using score
+        print(nums)
+        return nums
